ebf031ac51602210738886ade4a3625ffff3dac1
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2021-06-05 17:59:58 +0500

    Added music to hard coded music

--- music.js
+++ music.js
@@ -1,2 +1,16 @@
+var playing = false;
 var audio = new Audio('music.mp3');
-audio.play();
+
+function play_pause(){
+    var play_button = document.querySelector('#play')
+    if(playing){
+        audio.pause();
+        play_button.style.backgroundImage = "url('images/play.svg')"
+        playing = false;
+    }
+    else{
+        audio.play();
+        play_button.style.backgroundImage = "url('images/pause.svg')"
+        playing = true;
+    }
+}
