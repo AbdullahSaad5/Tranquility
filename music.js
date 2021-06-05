a088dd81db2a18e27456f4a99e7d2bdb70c96307
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2021-06-05 20:38:37 +0500

    Added new song

--- music.js
+++ music.js
@@ -1,11 +1,11 @@
 var playing = false;
-var audio = new Audio('music.mp3');
+var audio = new Audio('brown_munday.mp3');
 
 function play_pause(){
     var play_button = document.querySelector('#play')
 
     document.querySelector('#ending').innerHTML = convertTime(audio.duration);
-    document.getElementById("render").style.animationDuration = audio.duration.toString();
+    document.getElementById("render").style.animationDuration = audio.duration.toString() +"s";
     if(playing){
         document.getElementById("cd").style.animationPlayState = 'paused';
         document.getElementById("render").style.animationPlayState = 'paused';
