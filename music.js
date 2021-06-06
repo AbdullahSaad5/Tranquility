0917398df18679f031b0a3d42a647e07fd287b05
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2021-06-06 15:40:35 +0500

    Added new song

--- music.js
+++ music.js
@@ -1,40 +1,60 @@
-var playing = false;
-var audio = new Audio('brown_munday.mp3');
+    var audio = new Audio('Thoughts.mp3');
+    var play_button;
 
-function play_pause(){
-    var play_button = document.querySelector('#play')
+    function play_pause() {
+        play_button = document.querySelector('#play')
+        document.querySelector('#ending').innerHTML = convertTime(audio.duration);
+        document.getElementById("render").style.animationDuration = audio.duration.toString() + "s";
+        document.querySelector("#song-name p").innerHTML = audio.src;
 
-    document.querySelector('#ending').innerHTML = convertTime(audio.duration);
-    document.getElementById("render").style.animationDuration = audio.duration.toString() +"s";
-    if(playing){
-        document.getElementById("cd").style.animationPlayState = 'paused';
-        document.getElementById("render").style.animationPlayState = 'paused';
-
-        audio.pause();
-        play_button.style.backgroundImage = "url('images/play.svg')"
-        playing = false;
+        if (!audio.paused) {
+            pause();
+        } else {
+            play();
+        }
     }
-    else{
-        document.getElementById("cd").style.animationPlayState = 'running';
-        document.getElementById("render").style.animationPlayState = 'running';
 
-        setInterval(function(){ 
+    function play() {
+        if (Math.floor(audio.currentTime) == Math.floor(audio.duration)) {
+            document.getElementById("cd").style.animationPlayState = 'paused';
+            document.getElementById("render").style.animationPlayState = 'paused';
+        } else {
+            document.getElementById("cd").style.animationPlayState = 'running';
+            document.getElementById("render").style.animationPlayState = 'running';
+        }
+        setInterval(function () {
             var current = audio.currentTime;
-            document.getElementById("starting").innerHTML= convertTime(current)}, 1000);
+            document.getElementById("starting").innerHTML = convertTime(current)
+        }, 1000);
         audio.play();
         play_button.style.backgroundImage = "url('images/pause.svg')"
-        playing = true;
-    }
-}
-
-
-function convertTime(time){
-    var  minutes = Math.floor(time / 60).toString();
-    var  seconds = Math.floor(time % 60).toString();
-    if(seconds.length == 1){
-        seconds = 0 + seconds
     }
 
-    return minutes + ":" + seconds;
-}
 
+    function pause() {
+        document.getElementById("cd").style.animationPlayState = 'paused';
+        document.getElementById("render").style.animationPlayState = 'paused';
+        audio.pause();
+        play_button.style.backgroundImage = "url('images/play.svg')"
+    }
+
+
+    function skip() {
+        audio.paused = true;
+        play_pause();
+    }
+
+    function revert() {
+        audio.paused = true;
+        play_pause();
+    }
+
+    function convertTime(time) {
+        var minutes = Math.floor(time / 60).toString();
+        var seconds = Math.floor(time % 60).toString();
+        if (seconds.length == 1) {
+            seconds = 0 + seconds
+        }
+
+        return minutes + ":" + seconds;
+    }
