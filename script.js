96e7ebdf05798869bde601eade54b61683c83e28
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2021-06-06 18:58:00 +0500

    Added swet alert to login page

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function getCredentials() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    
+
     if (email == "test@test.com" && password == "test123") {
-        alert("Login Successful")
-        window.location.href = "music.html"
-    }
-    else {
-        alert("Invalid Username or Password")
+        // alert("Login Successful");
+        swal("Sucess!", "Click OK to continue!", "success").then(function () {
+            window.location.href = "music.html";
+        });
+    } else {
+        // alert("Invalid Username or Password");
+        swal("Error!", "Invald Username or Password", "error");
     }
 }
