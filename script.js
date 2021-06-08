4c57ee5932df85394d9ce22a850bdb725d1bb8d6
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2021-06-08 15:18:01 +0500

    Fixed login error

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-var logged_in = false;
-
 function getCredentials() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
     if (email == "test@test.com" && password == "test123") {
-        // alert("Login Successful");
-        logged_in = true;
         swal("Success!", "Click OK to continue!", "success").then(function () {
             window.location.href = "music.html";
         });
     } else {
-        // alert("Invalid Username or Password");
         swal("Error!", "Invalid Username or Password", "error");
     }
-    export default logged_in;
 }
