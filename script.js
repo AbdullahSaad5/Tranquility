26dc69c0c68ca73476e7f0b0c112f2711ffd5d3e
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2021-08-06 21:41:44 +0500

    made minor modifications

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function getCredentials() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+const loginButton = document.getElementById("submit");
 
-    if (email == "test@test.com" && password == "test123") {
-        swal("Logged In!", "Click OK to continue!", "success").then(function () {
-            window.location.href = "music.html";
-        });
-    } else {
-        swal("Error!", "Invalid Username or Password", "error");
-    }
+function getCredentials() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  if (email == "test@test.com" && password == "test123") {
+    swal("Logged In!", "Click OK to continue!", "success").then(function () {
+      window.location.href = "music.html";
+    });
+  } else {
+    swal("Error!", "Invalid Username or Password", "error");
+  }
 }
+
+loginButton.addEventListener("click", getCredentials);
